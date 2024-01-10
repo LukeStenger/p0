601600387d2b44fd60ab68cf4480db45b6752175
Luke Stenger <lstenger7@gmail.com>
2024-01-10 14:33:39 -0500

    added additional feature

--- groupme-bot/bot.py
+++ groupme-bot/bot.py
@@ -9,8 +9,8 @@ load_dotenv()
 BOT_ID = os.getenv("BOT_ID")
 GROUP_ID = os.getenv("GROUP_ID")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+SENDER_ID = os.getenv("SENDER_ID")
 LAST_MESSAGE_ID = None
-
 
 def send_message(text, attachments=None):
     """Send a message to the group using the bot."""
@@ -33,27 +33,52 @@ def get_group_messages(since_id=None):
         return response.json().get("response", {}).get("messages", [])
     return []
 
+def like_message(message_id):
+    """Like a specific message."""
+    like_url = f"https://api.groupme.com/v3/messages/{GROUP_ID}/{message_id}/like?token={ACCESS_TOKEN}"
+    response = requests.post(like_url)
+    return response.status_code == 200
+
 
 def process_message(message):
     """Process and respond to a message."""
     global LAST_MESSAGE_ID
     text = message["text"].lower()
+    user = message["user_id"]
+    send = message["sender_id"]
+    sender_type = message["sender_type"]
+    user_name = message["name"]
 
-    # i.e. responding to a specific message (note that this checks if "hello bot" is anywhere in the message, not just the beginning)
-    if "hello bot" in text:
-        send_message("sup")
+    if sender_type != "bot" and LAST_MESSAGE_ID is not None:
+
+        # i.e. responding to a specific message (note that this checks if "hello bot" is anywhere in the message, not just the beginning)
+        if "hello bot" in text and user == SENDER_ID:
+            send_message("sup")
+            
+
+        if message["sender_type"] != "bot" and "good morning" in text:
+            send_message(f"Good morning, {user_name}!")
+
+        if message["sender_type"] != "bot" and "good night" in text:
+            send_message(f"Good night, {user_name}!")
+
+        if "like this message" in text:
+            like_message(message["id"])
 
     LAST_MESSAGE_ID = message["id"]
 
 
 def main():
     global LAST_MESSAGE_ID
+    global LAST_MESSAGE_TIMESTAMP
     # this is an infinite loop that will try to read (potentially) new messages every 10 seconds, but you can change this to run only once or whatever you want
+
     while True:
         messages = get_group_messages(LAST_MESSAGE_ID)
         for message in reversed(messages):
             process_message(message)
         time.sleep(10)
+
 
 
 if __name__ == "__main__":
